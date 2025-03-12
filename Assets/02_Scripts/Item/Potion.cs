a7ec8cc8a81014d637c7f75d6b2d1223b1304147
Xenawn <codeparadoxist@gmail.com>
2025-03-12 16:42:59 +0900

    [Feat] 포션 스크립트 생성

---
+++ Assets/02_Scripts/Item/Potion.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+interface IEffect
+{
+    public void Use();
+}
+public class Potion : MonoBehaviour,IEffect
+{
+  
+    public void Use()
+    {
+       
+    }
+
+    // Start is called before the first frame update
+  
+}
