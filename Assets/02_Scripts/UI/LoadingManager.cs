5f0b135dbc7ccd3fddc3625e71e28cc841cb1804
KwonSumin <rbalstnals1920@gmail.com>
2025-03-17 12:10:47 +0900

    Feat TitleScene 및 LoadingScene구현

--- Assets/02_Scripts/UI/LoadingManager.cs
+++ Assets/02_Scripts/UI/LoadingManager.cs
@@ -19,20 +19,26 @@ public class LoadingManager : MonoBehaviour
         AsyncOperation operation = SceneManager.LoadSceneAsync("GameScene");
         operation.allowSceneActivation = false;
 
-        float fakeProgress = 0f;
+        float fakeProgress = 0f; // 가짜 진행도
         float minLoadingTime = 3f; // 최소 로딩 시간
         float elapsedTime = 0f;
 
         while (!operation.isDone)
         {
-            // 실제 progress 값과 가짜 progress 값을 비교하여 증가
+            // 실제 progress 값 (0~0.9)
             float realProgress = Mathf.Clamp01(operation.progress / 0.9f);
-            fakeProgress = Mathf.Lerp(fakeProgress, realProgress, Time.deltaTime * 2f);
-            progressBar.value = fakeProgress;
+
+            //  fakeProgress가 realProgress보다 빨리 증가하지 않도록 설정
+            if (fakeProgress < realProgress)
+            {
+                fakeProgress = Mathf.Lerp(fakeProgress, realProgress, Time.deltaTime * 2f);
+            }
+
+            progressBar.value = fakeProgress; // 로딩 바 업데이트
 
             elapsedTime += Time.deltaTime;
 
-            // 최소 로딩 시간이 지나기 전까지 씬 전환 X
+            // 최소 로딩 시간이 지나고, 실제 로딩이 끝났을 경우에만 씬 전환
             if (realProgress >= 0.9f && elapsedTime >= minLoadingTime)
             {
                 yield return new WaitForSeconds(1f);
